15a7f3feaeafeabcf5988ca3e939fe9eb23e0a94
Lukasz Baran <lukasz.baran@gmail.com>
2016-06-14 16:27:08 +0200

    javascript stuff

--- starter-watcher/src/main/webapp/js/starter.js
+++ starter-watcher/src/main/webapp/js/starter.js
@@ -1,4 +1,31 @@
 var APP = angular.module('APP', []);
 APP.ApplicationCtrl = function ($scope) {
-    $scope.name = 'World';
+    $scope.name = '...';
+
+    $scope.sayHelloWorld = function() {
+        $scope.name = 'World';
+    }
 };
+
+APP.factory('Data', function() {
+    return {message:"I'm data from a service"}
+})
+
+function firstCtrl($scope, Data) {
+	$scope.data = Data;
+}
+
+function secondCtrl($scope, Data) {
+	$scope.data = Data;
+
+	$scope.reversedMessage = function() {
+		return $scope.data.message.split("").reverse().join("");
+	}
+}
+
+APP.controller('usersCtrl', function($scope, $http) {
+    $scope.loadUsers = function() {
+        //$http.get() TODO
+
+    }
+});
